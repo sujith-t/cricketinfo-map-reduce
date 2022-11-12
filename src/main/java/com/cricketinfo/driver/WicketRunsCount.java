3b8c57cfc21a4c91cc50c19d3e78ecd5d4956caa
sujith-t <exodus123@gmail.com>
2022-11-12 19:46:20 +0530

    test commit

--- src/main/java/com/cricketinfo/driver/WicketRunsCount.java
+++ src/main/java/com/cricketinfo/driver/WicketRunsCount.java
@@ -3,6 +3,6 @@ package com.cricketinfo.driver;
 public class WicketRunsCount {
 
     public static void main(String[] args) {
-        System.out.println("hello user");
+        System.out.println("hello user1");
     }
 }
